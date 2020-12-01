deb8ec7c6155bb60f007c982409c280d62753598
Cassandra Durnford <illegalhex@icloud.com>
2020-12-01 17:40:21 -0500

    house cleaning

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,5 @@ pub fn find_2020_pair(vals: Vec<i32> ) -> Vec<i32> {
         }
     i -= 1
     }
-
-    // for i in vals.into_iter() {
-    //     for n in 1..vals_len{
-    //         println!("in{}, out{}", n, i)
-    //     }
-    //     vals_len -= 1
-    //     vals
-    // }
     vec![1,2]
 }
