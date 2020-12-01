0dd122b1a6b430e42e4dc3904973e412afa55422
Cassandra Durnford <illegalhex@icloud.com>
2020-12-01 17:32:07 -0500

    refactor since I'll need to use retain on vector

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,20 @@
 
 pub fn find_2020_pair(vals: Vec<i32> ) -> Vec<i32> {
     let vals_len = vals.len();
-    for i in vals.into_iter() {
-        for n in 1..vals_len{
-            println!("in{}, out{}", n, i)
+    let mut i = vals_len;
+    while i > 0 { 
+        for n in vals.iter() { 
+            println!("in{}, out{}", i, n)
         }
-        // println!("{}", i)
+    i -= 1
     }
+
+    // for i in vals.into_iter() {
+    //     for n in 1..vals_len{
+    //         println!("in{}, out{}", n, i)
+    //     }
+    //     vals_len -= 1
+    //     vals
+    // }
     vec![1,2]
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
 fn main() {
-    // println!("Hello, world!");
-    adventofcode_2020::find_2020_pair(vec![1,2,3,4]);
+    println!("Hello, world!");
 }
