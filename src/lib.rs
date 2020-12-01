4d047477dd869968e0995a4f7d2fdf4feb78b9fe
Cassandra Durnford <illegalhex@icloud.com>
2020-12-01 17:33:59 -0500

    off by 1

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,10 @@
 
 pub fn find_2020_pair(vals: Vec<i32> ) -> Vec<i32> {
     let vals_len = vals.len();
-    let mut i = vals_len;
+    let mut i = vals_len - 1 ;
     while i > 0 { 
         for n in vals.iter() { 
-            println!("in{}, out{}", i, n)
+            println!("in{}, out{}", vals[i], n)
         }
     i -= 1
     }
