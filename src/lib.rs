aae38d3c628f2e19ab26ce29e171628db8a3c915
Cassandra Durnford <illegalhex@icloud.com>
2020-12-01 20:03:02 -0500

    tests refactored

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 use itertools::Itertools;
 
-pub fn find_2020_pair(vals: Vec<i32> ) -> i32 {
+pub fn find_2020_pair(vals_in: (usize, Vec<i64>) ) -> i64 {
+    let vals = vals_in.1;
     let mut value = 0;
-    let val_pairs = vals.into_iter().tuple_combinations::<(_, _)>();
+    let val_pairs = vals.into_iter().combinations(vals_in.0);
     for i in val_pairs.into_iter() {
-        if i.0 + i.1 == 2020 {
-            value = i.0 * i.1;
+        // println!("{:#?}", i);
+        if i.iter().sum::<i64>() == 2020 {
+            println!("{:#?}", i);
+            value = i.iter().product();
         }
     }
     value
