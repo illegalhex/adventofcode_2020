0319ee381e7f8a67b859fe5e637a9c8698e6db07
Cassandra Durnford <illegalhex@icloud.com>
2020-12-01 19:34:22 -0500

    green case

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@ pub fn find_2020_pair(vals: Vec<i32> ) -> i32 {
     let val_pairs = vals.into_iter().tuple_combinations::<(_, _)>();
     for i in val_pairs.into_iter() {
         if i.0 + i.1 == 2020{
-            println!("{},{}", i.0, i.1)
+            println!("{},{}", i.0, i.1);
+            return i.0 * i.1
         }
     }
     return 514579
