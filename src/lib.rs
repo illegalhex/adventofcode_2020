555606ea55604716fed36ad2570bc8506b0b6c2b
Cassandra Durnford <illegalhex@icloud.com>
2020-12-01 19:42:28 -0500

    ex1 done

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
 use itertools::Itertools;
 
 pub fn find_2020_pair(vals: Vec<i32> ) -> i32 {
-
+    let mut value = 0;
     let val_pairs = vals.into_iter().tuple_combinations::<(_, _)>();
     for i in val_pairs.into_iter() {
-        if i.0 + i.1 == 2020{
-            println!("{},{}", i.0, i.1);
-            return i.0 * i.1
+        if i.0 + i.1 == 2020 {
+            value = i.0 * i.1;
         }
     }
-    return 514579
+    value
 }
