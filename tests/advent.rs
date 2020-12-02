dbda19a8870938bce8ae7d2b926f0c484b84f10e
Cassandra Durnford <illegalhex@icloud.com>
2020-12-01 19:49:13 -0500

    red testing for ex2

--- tests/advent.rs
+++ tests/advent.rs
@@ -2,10 +2,19 @@
 // day1
 #[test]
 fn find_2020_pair(){
-    assert_eq!(adventofcode_2020::find_2020_pair(vec![1721,
+    assert_eq!(adventofcode_2020::find_2020_pair((2, vec![1721,
         979,
         366,
         299,
         675,
-        1456]), 514579)
+        1456])), 514579)
+}
+#[test]
+fn find_2020_triplets(){
+    assert_eq!(adventofcode_2020::find_2020_pair((3, vec![1721,
+        979,
+        366,
+        299,
+        675,
+        1456])), 241861950)
 }
