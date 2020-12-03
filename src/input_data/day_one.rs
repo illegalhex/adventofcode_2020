47f9b324a62b99dfb0f021a068766655db06b14c
Cassandra Durnford <illegalhex@icloud.com>
2020-12-03 02:20:58 -0500

    f

--- src/input_data/day_one.rs
+++
@@ -1,5 +0,0 @@
-#[allow(dead_code)]
-pub fn print(print_vec: Vec<String>) -> Vec<usize> {
-    let usize_print_vec = print_vec.into_iter().map(|letter| { letter.parse::<usize>().unwrap() }).collect();
-    return usize_print_vec
-}
