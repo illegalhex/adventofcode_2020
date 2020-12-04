c1a97b62189e6a4e098e55cdc26a078f29f519ab
Cassandra Durnford <illegalhex@icloud.com>
2020-12-04 14:27:55 -0500

    data format valid

--- src/input_data.rs
+++ src/input_data.rs
@@ -1,5 +1,7 @@
-pub mod print{
 
+
+pub mod print{
+    use std::collections::HashMap;
     pub fn print_day1(print_vec: Vec<String>) -> Vec<usize> {
         let usize_print_vec = print_vec.into_iter().map(|letter| { letter.parse::<usize>().unwrap() }).collect();
         return usize_print_vec
@@ -20,4 +22,24 @@ pub mod print{
         }
         return return_vec
     }
+
+    pub fn print_day4(print_vec: Vec<String>) -> Vec<HashMap<String, String>>{
+        // This will take in the unmodified starting text, and return a vector of hash maps
+        // that match the defined data structure
+        let mut return_vec: Vec<_>  = Vec::new();
+        let mut massive_string: String = "".to_string();
+        for i in print_vec.into_iter() {if i == ""{massive_string.push_str(",");continue;}massive_string.push_str(&i);}
+        let first_level_vec: Vec<&str>  = massive_string.split(|x| x == ',').collect();
+        for i in first_level_vec.into_iter(){
+            let temp_vec = i.split_whitespace().collect::<Vec<&str>>();
+            let mut temp_hash: HashMap<String, String> = HashMap::new();
+            for i in temp_vec.into_iter(){
+                let vec_i: Vec<&str> = i.split(|x: char| x.to_string() == ":").collect();
+                temp_hash.insert(vec_i[0].to_string(),vec_i[1].to_string());
+            }
+            return_vec.push(temp_hash);
+        }
+        return_vec
+    }
+    
 }   
