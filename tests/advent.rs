b2ac1fac2315dfddb80201d359aadea48e170c10
Cassandra Durnford <illegalhex@icloud.com>
2020-12-04 00:59:12 -0500

    init day 4

--- tests/advent.rs
+++ tests/advent.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 
 // day1
 #[test]
@@ -93,4 +94,51 @@ fn find_multislopes_trees_product(){
     let fifth = adventofcode_2020::count_of_trees(1, 2, return_tree_map());
 
     assert_eq!(first * second * third * forth * fifth, 336);
+}
+
+fn return_passports() -> Vec<HashMap<String, String>> {
+    let mut one =  HashMap::new();
+    let mut two =  HashMap::new();
+    let mut three =  HashMap::new();
+    let mut four =  HashMap::new();
+
+    one.insert("ecl".to_string(),"gry".to_string());
+    one.insert("pid".to_string(),"860033327".to_string());
+    one.insert("eyr".to_string(),"2020".to_string());
+    one.insert("hcl".to_string(),"#fffffd".to_string());
+    one.insert("byr".to_string(),"1937".to_string());
+    one.insert("iyr".to_string(),"2017".to_string());
+    one.insert("cid".to_string(),"147".to_string());
+    one.insert("hgt".to_string(),"183cm".to_string());
+
+    two.insert("iyr".to_string(),"2013".to_string());
+    two.insert("ecl".to_string(),"amb".to_string());
+    two.insert("cid".to_string(),"350".to_string());
+    two.insert("eyr".to_string(),"2023".to_string());
+    two.insert("pid".to_string(),"028048884".to_string());
+    two.insert("hcl".to_string(),"#cfa07d".to_string());
+    two.insert("byr".to_string(),"1929".to_string());
+
+    three.insert("hcl".to_string(),"#ae17e1".to_string());
+    three.insert("iyr".to_string(),"2013".to_string());
+    three.insert("eyr".to_string(),"2024".to_string());
+    three.insert("ecl".to_string(),"brn".to_string());
+    three.insert("pid".to_string(),"760753108".to_string());
+    three.insert("byr".to_string(),"1931".to_string());
+    three.insert("hgt".to_string(),"179cm".to_string());
+
+    four.insert("hcl".to_string(),"#cfa07d".to_string());
+    four.insert("eyr".to_string(),"2025".to_string());
+    four.insert("pid".to_string(),"166559648".to_string());
+    four.insert("iyr".to_string(),"2011".to_string());
+    four.insert("ecl".to_string(),"brn".to_string());
+    four.insert("hgt".to_string(),"59in".to_string());
+
+    return vec![one, two, three, four]
+}
+
+#[test]
+fn find_number_valid_passports(){
+    println!("{:?}", return_passports());
+    assert_eq!(true, true)
 }
